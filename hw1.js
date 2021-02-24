0d1434b8bd895ec1b58077326621914416f768d9
Buck Pendleton <buckpendleton@gmail.com>
2021-02-23 23:33:22 -0500

    Week 6 HW

---
+++ hw1.js
@@ -0,0 +1,32 @@
+var navbar = document.getElementById("navbar");
+console.log(navbar);
+
+
+var showcase = document.getElementById("showcase");
+console.log(showcase);
+
+
+var footer = document.getElementById("footer");
+console.log(footer);
+
+
+var footerNodes = footer.childNodes;
+console.log(footerNodes);
+
+
+var footerChildren = footer.children;
+console.log(footerChildren);
+
+
+var navLinks = document.getElementsByClassName("nav-link");
+console.log(navLinks);
+
+
+var ul = document.querySelector("ul");
+console.log(ul);
+
+
+console.log(ul.children);
+var aboutLi = ul.children[1];
+console.log(aboutLi);
+
